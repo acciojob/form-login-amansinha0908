39d8837969695c72816efaccd31e54345e89ceff
amansinha0908 <139490539+amansinha0908@users.noreply.github.com>
2025-09-26 11:31:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function getFormvalue() {
     //Write your code here
 	
-	let fname = document.querySelector('input [name="fname"]').value.trim;
-      let lname = document.querySelector('input [name="lname"]').value.trim;
+	let fname = document.querySelector('input[name="fname"]').value.trim();
+      let lname = document.querySelector('input[name="lname"]').value.trim();
 
 	alert(fname + " " + lname);
 
